8f7e629b8cf5c2c49c2e59840477d39b40fbd9cd
Matte134 <MartinBusse87@gmail.com>
2018-01-25 04:06:48 +0100

    Wichtigste Feinanpssaungen + header- joblinks -smoothscroll
    Fehlt: Transitions noch optimierbar(einblendeffekt fehlt), Navigationsaktivierung bei jeweiliger section fehlt und link bei Jobs nicht über ganzem Div
    Dafür:
    Header stark bearbeitet mit border bottom in richtiger position burger icon und richtigem Verlauf. Datenschutz und Impressum angepasst, HEader Positionen und Links passen jetzt überall. Smooth scroll dank commander richtig. Das plus minuas symbol  bei den Jobs passt.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+(function(a,c){var b=(function(){var d=c(a.documentElement),f=c(a.body),e;if(d.scrollTop()){return d}else{e=f.scrollTop();if(f.scrollTop(e+1).scrollTop()==e){return d}else{return f.scrollTop(e)}}}());c.fn.smoothScroll=function(d){d=~~d||400;return this.find('a[href*="#"]').click(function(f){var g=this.hash,e=c(g);if(location.pathname.replace(/^\//,'')===this.pathname.replace(/^\//,'')&&location.hostname===this.hostname){if(e.length){f.preventDefault();b.stop().animate({scrollTop:e.offset().top},d,function(){location.hash=g})}}}).end()}}(document,jQuery));
+
+jQuery(document).ready(function() {
+
+  $('html').smoothScroll();
+
+  $('.navbar .nav-item a').click(function() {
+      $('.navbar .nav-item a').removeClass('clicked');
+      $(this).addClass('clicked');
+
+  })
+
+  var waypoint = new Waypoint({
+  element: jQuery('#projekte'),
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!')
+  }
+})
+
+
+
+});
 
 function main() {
 
@@ -6,19 +28,19 @@ function main() {
 
 	// Hide .navbar first
 	//$(".navbar").hide();
-	
+
 	// Fade in .navbar
-		
+
 	// Preloader */
 	  	$(window).load(function() {
 
-   	// will first fade out the loading animation 
-    	$("#status").fadeOut("slow"); 
+   	// will first fade out the loading animation
+    	$("#status").fadeOut("slow");
 
-    	// will fade out the whole DIV that covers the website. 
-    	$("#preloader").delay(500).fadeOut("slow").remove();      
+    	// will fade out the whole DIV that covers the website.
+    	$("#preloader").delay(500).fadeOut("slow").remove();
 
-  	}) 
+  	})
 
    // Page scroll
   	$('a.page-scroll').click(function() {
@@ -44,13 +66,20 @@ function main() {
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     })
 
   	$(document).ready(function() {
-  	    $("#testimonial").owlCarousel({
+
+      /*! http://mths.be/smoothscroll v1.5.2 by @mathias */
+
+
+
+
+
+        $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
@@ -86,26 +115,77 @@ function main() {
         });
 
     });
-	
-	
+
+
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+//Smooth scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
   // jQuery Parallax
   function initParallax() {
     $('#intro').parallax("100%", 0.3);
-    $('#services').parallax("100%", 0.3);
-    $('#aboutimg').parallax("100%", 0.3);	
-    $('#testimonials').parallax("100%", 0.1);
+    $('#Philosophie').parallax("100%", 0.3);
+    $('#jobs').parallax("100%", 0.3);
+    $('#Kontakt').parallax("100%", 0.1);
 
   }
   initParallax();
 
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
 
 }
-main();
